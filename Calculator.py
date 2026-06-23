c8c07861e2bb59dd3b5f4dadc4d7fa1767824dd0
SihuPark <sihuop0823@gmail.com>
2026-06-24 01:18:39 +0900

    사칙연산 우선순위

--- Calculator.py
+++ Calculator.py
@@ -1,35 +1,50 @@
 expression = input()
 
+exStack = []
 res = 0
 num = ""
 op = "+"
 
 for ch in expression:
     if ch.isdigit():
+
         num += ch
+
     else:
-        if num != "":
-            if op == "+":
-                res += int(num)
-            elif op == "-":
-                res -= int(num)
-            elif op == "*":
-                res *= int(num)
-            elif op == "/":
-                res /= int(num)
+        n = int(num)
+
+        if op == "+":
+            exStack.append(n)
+
+        elif op == "-":
+            exStack.append(-n)
+            
+        elif op == "*":
+            exStack.append(exStack.pop() * n)
+
+        elif op == "/":
+            exStack.append(exStack.pop() / n)
+        
+        # ㅁㅇ러ㅣㄴㅁ런ㄹ 
+        # stack에 쌓이면 어 무튼 1 + 2 * 3 이면 1 2 * 3 먼저 계산이니까 2먼저 들어오니까 1 , 6 되고
+        # sum으로 더하고 
 
         op = ch
         num = ""
 
 if num != "":
-    if op == "+":
-        res += int(num)
-    elif op == "-":
-        res -= int(num)
-    elif op == "*":
-        res *= int(num)
-    elif op == "/":
-        res /= int(num)
-    
+    n = int(num)
 
-print(res)
+    if op == "+":
+        exStack.append(n)
+
+    elif op == "-":
+        exStack.append(-n)
+
+    elif op == "*":
+        exStack.append(exStack.pop() * n)
+
+    elif op == "/":
+        exStack.append(exStack.pop() / n)
+
+print(sum(exStack))
