d29f6b75a573a1a979955ee01451b368869cec60
SihuPark <sihuop0823@gmail.com>
2026-06-24 00:25:55 +0900

    간단한 사칙연산 (순서 예외 처리 x)

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,35 @@
+expression = input()
+
+res = 0
+num = ""
+op = "+"
+
+for ch in expression:
+    if ch.isdigit():
+        num += ch
+    else:
+        if num != "":
+            if op == "+":
+                res += int(num)
+            elif op == "-":
+                res -= int(num)
+            elif op == "*":
+                res *= int(num)
+            elif op == "/":
+                res /= int(num)
+
+        op = ch
+        num = ""
+
+if num != "":
+    if op == "+":
+        res += int(num)
+    elif op == "-":
+        res -= int(num)
+    elif op == "*":
+        res *= int(num)
+    elif op == "/":
+        res /= int(num)
     
+
+print(res)
