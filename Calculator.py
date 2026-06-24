1fa90882b1c3e1ede105db08b751e8f23d43d3d6
SihuPark <sihuop0823@gmail.com>
2026-06-24 20:31:12 +0900

    음수 처리 구현

--- Calculator.py
+++ Calculator.py
@@ -7,6 +7,8 @@ parenthesisStack = []
 
 n = ""
 op = "+"
+plusminusSign = 1
+
 
 if re.search(r'[가-힣a-zA-Z]', expression):
     print("연산할 수 없는 문자가 포함되어 있음")
@@ -29,30 +31,39 @@ for ch in expression:
 
     elif ch in "+-*/":
         if n == "":
+            if ch == "-":
+                plusminusSign = -1
+                continue
+            elif ch == "+":
+                plusminusSign = 1
+                continue
             print("잘못된 수식입니다")
             exit()
 
-        apply(exStack, op, int(n))
+        apply(exStack, op, int(n) * plusminusSign)
         n = ""
+        plusminusSign = 1
         op = ch
 
     elif ch == "(":
         parenthesisStack.append((exStack, op))
         exStack = []
         op = "+"
+        plusminusSign = 1
         n = ""
 
     elif ch == ")":
         if n == "":
-            print("잘못된 수식입니다")
+            print("어딘가 잘못된 수식입니다")
             exit()
 
-        apply(exStack, op, int(n))
+        apply(exStack, op, int(n) * plusminusSign)
 
         result = sum(exStack)
 
         exStack, op = parenthesisStack.pop()
         n = str(result)
+        plusminusSign = 1
 
 if n != "":
     apply(exStack, op, int(n))
@@ -61,15 +72,17 @@ print(sum(exStack))
 
 
 '''
-1. 처음에 문자열(한글, 영어) 들어가 있는지 검사
+1. 문자열에 한글, 영어가 있는지 검사
 
-2. 글자 한글자씩 돌면서 숫자면 num에 붙임
+2. 숫자는 n에 붙이고 연산자를 만나면 이전 연산자로 계산
 
-3. 연산자 만나면 이전 연산자에 따라 계산
+3. *, / 는 바로 계산하고 +, - 는 스택에 저장
 
-4. * / 면 스택에 가장 최근에 들어온 애랑 먼저 계산
+4. ( 를 만나면 현재 상태를 저장하고 괄호 안 계산 시작
 
-5. 마지막 숫자 처리하고 스택에 있는 거 전부 더해서 계산
+5. ) 를 만나면 괄호 안 결과를 구해서 괄호 밖 계산에 이어서 사용
+
+6. 마지막 숫자를 처리하고 스택 합계를 출력
 '''
 
 
