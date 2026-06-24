3ac95043711f7a5c6602c3a8b52e0e8fa3850d06
SihuPark <sihuop0823@gmail.com>
2026-06-24 20:11:09 +0900

    양수 괄호 구현

--- Calculator.py
+++ Calculator.py
@@ -1,56 +1,75 @@
 import re
 
-expression = input()
+expression = input().replace(" ", "")
 
 exStack = []
-res = 0
-num = ""
-op = "+"
-excep = re.compile(r'[가-힣a-zA-Z]')
+parenthesisStack = []
 
-if excep.match(expression):
-    print("연산할 수 없는 문자가 포함되어 있습니다")
+n = ""
+op = "+"
+
+if re.search(r'[가-힣a-zA-Z]', expression):
+    print("연산할 수 없는 문자가 포함되어 있음")
+    exit()
+
+def apply(stack, op, n):
+    if op == "+":
+        stack.append(n)
+    elif op == "-":
+        stack.append(-n)
+    elif op == "*":
+        stack.append(stack.pop() * n)
+    elif op == "/":
+        stack.append(stack.pop() / n)
+
 
 for ch in expression:
     if ch.isdigit():
+        n += ch
 
-        num += ch
+    elif ch in "+-*/":
+        if n == "":
+            print("잘못된 수식입니다")
+            exit()
 
-    else:
-        n = int(num)
-
-        if op == "+":
-            exStack.append(n)
-
-        elif op == "-":
-            exStack.append(-n)
-            
-        elif op == "*":
-            exStack.append(exStack.pop() * n)
-
-        elif op == "/":
-            exStack.append(exStack.pop() / n)
-        
-        # ㅁㅇ러ㅣㄴㅁ런ㄹ 
-        # stack에 쌓이면 어 무튼 1 + 2 * 3 이면 1 2 * 3 먼저 계산이니까 2먼저 들어오니까 1 , 6 되고
-        # sum으로 더하고 
-
+        apply(exStack, op, int(n))
+        n = ""
         op = ch
-        num = ""
 
-if num != "":
-    n = int(num)
+    elif ch == "(":
+        parenthesisStack.append((exStack, op))
+        exStack = []
+        op = "+"
+        n = ""
 
-    if op == "+":
-        exStack.append(n)
+    elif ch == ")":
+        if n == "":
+            print("잘못된 수식입니다")
+            exit()
 
-    elif op == "-":
-        exStack.append(-n)
+        apply(exStack, op, int(n))
 
-    elif op == "*":
-        exStack.append(exStack.pop() * n)
+        result = sum(exStack)
 
-    elif op == "/":
-        exStack.append(exStack.pop() / n)
+        exStack, op = parenthesisStack.pop()
+        n = str(result)
+
+if n != "":
+    apply(exStack, op, int(n))
 
 print(sum(exStack))
+
+
+'''
+1. 처음에 문자열(한글, 영어) 들어가 있는지 검사
+
+2. 글자 한글자씩 돌면서 숫자면 num에 붙임
+
+3. 연산자 만나면 이전 연산자에 따라 계산
+
+4. * / 면 스택에 가장 최근에 들어온 애랑 먼저 계산
+
+5. 마지막 숫자 처리하고 스택에 있는 거 전부 더해서 계산
+'''
+
+
